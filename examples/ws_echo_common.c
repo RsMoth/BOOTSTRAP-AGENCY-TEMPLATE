a6fd185f9c80818260e7162949b8410c81351cb7
RsMoth <67247209+RsMoth@users.noreply.github.com>
2020-08-05 18:13:57 +0000

    2002-01-07  Michael Snyder  <msnyder@redhat.com>

--- examples/ws_echo_common.c
+++ examples/ws_echo_common.c
@@ -42,3 +42,81 @@ char *create_root_response(int port, int count) {
       "    var count = %d;\n"
       "    ws.onopen = function() {\n"
       "      alert(\"Sending \"+count);\n"
+      "      ws.send(\"count[\"+count+\"]\");\n"
+      "    };\n"
+      "    ws.onmessage = function (evt) {\n"
+      "      alert(\"Received (\"+evt.data+\"), sending \"+\n"
+      "           (count > 1 ? (count-1) : \"close\"));\n"
+      "      if (count > 1) {\n"
+      "        ws.send(\"count[\"+(--count)+\"]\");\n"
+      "      } else {\n"
+      "        ws.close();\n"
+      "      }\n"
+      "    };\n"
+      "    ws.onclose = function() { alert(\"Closed\"); };\n"
+      "    ws.onerror = function(e) { alert(\"Error: \"+e.data); };\n"
+      "  } else {\n"
+      "    alert(\"WebSocket NOT supported by your Browser!\");\n"
+      "  }\n"
+      "}\n"
+      "</script></head><body><div id=\"sse\">\n"
+      "  <a href=\"javascript:WebSocketTest()\">Run WebSocket</a>\n"
+      "</div></body></html>\n", port, count) < 0) {
+    return NULL;  // asprintf failed
+  }
+  char *ret = NULL;
+  if (asprintf(&ret,
+      "HTTP/1.1 200 OK\r\n"
+      "Content-length: %zd\r\n"
+      "Connection: close\r\n"
+      "Content-Type: text/html; charset=UTF-8\r\n"
+      "\r\n%s",
+      (html ? strlen(html) : 0), html) < 0) {
+    return NULL;  // asprintf failed
+  }
+  free(html);
+  return ret;
+}
+
+ws_status on_http_request(ws_t ws,
+    const char *method, const char *resource, const char *version,
+    const char *host, const char *headers, size_t headers_length,
+    bool is_websocket, bool *to_keep_alive) {
+  if (strcmp(method, "GET") || strcmp(resource, "/")) {
+    return WS_ERROR;
+  }
+  if (!is_websocket) {
+    char *data = create_root_response(((my_t)ws->state)->port, 3);
+    ws_status ret = ws->send_data(ws, data, strlen(data));
+    free(data);
+    return ret;
+  }
+  return WS_SUCCESS;
+}
+
+ws_status on_upgrade(ws_t ws,
+    const char *resource, const char *protocol,
+    int version, const char *sec_key) {
+  return ws->send_upgrade(ws);
+}
+
+ws_status on_frame(ws_t ws,
+    bool is_fin, uint8_t opcode, bool is_masking,
+    const char *payload_data, size_t payload_length,
+    bool *to_keep) {
+  switch (opcode) {
+    case OPCODE_TEXT:
+    case OPCODE_BINARY:
+      if (!is_fin) {
+        // wait for full data
+        *to_keep = true;
+        return WS_SUCCESS;
+      }
+      if (!is_masking) {
+        return ws->send_close(ws, CLOSE_PROTOCOL_ERROR,
+            "Clients must mask");
+      }
+      // echo
+      return ws->send_frame(ws,
+          true, opcode, false,
+          payload_data, payload_length);
