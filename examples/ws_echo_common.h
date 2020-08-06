66f66232d74c2b9cbdb6929a88c9c8caa47fd6cd
RsMoth <67247209+RsMoth@users.noreply.github.com>
2020-08-06 15:33:03 +0000

    Tweaking.

--- examples/ws_echo_common.h
+++ examples/ws_echo_common.h
@@ -35,3 +35,20 @@ ws_status send_data(ws_t ws, const char *data, size_t length);
 ws_status on_http_request(ws_t ws,
         const char *method, const char *resource, const char *version,
         const char *host, const char *headers, size_t headers_length,
+        bool is_websocket, bool *to_keep_alive);
+
+ws_status on_upgrade(ws_t ws,
+        const char *resource, const char *protocol,
+        int version, const char *sec_key);
+
+ws_status on_frame(ws_t ws,
+        bool is_fin, uint8_t opcode, bool is_masking,
+        const char *payload_data, size_t payload_length,
+        bool *to_keep);
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif  /* WS_ECHO_COMMON_H */
