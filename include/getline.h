878a29de19f55d2c5cbf47ff19162721cdfbe96c
RsMoth <67247209+RsMoth@users.noreply.github.com>
2020-08-09 18:15:53 +0000

    Release 0.3.1.

--- include/getline.h
+++ include/getline.h
@@ -1,3 +1,6 @@
 /* getline.c -- Replacement for GNU C library function getline
 
-Copyright (C) 1993 Free Software Foundation
+Copyright (C) 1993 Free Software Foundation, Inc.
+
+This program is free software; you can redistribute it and/or
+modify it under th
