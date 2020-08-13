7c21382674e142cfb82f5ec1b85c8a3b30a248aa
RsMoth <67247209+RsMoth@users.noreply.github.com>
2020-08-13 11:48:11 +0000

    ALSA: ice1724: Use linear scale for AK4396 volume control.

--- include/getline.h
+++ include/getline.h
@@ -4,4 +4,7 @@ Copyright (C) 1993 Free Software Foundation, Inc.
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License as
-published by the Free Software Foundation; eith
+published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
+
+This program is distr
