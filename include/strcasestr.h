348a0487d9b90cd2d268db023efcb2492bd6314f
RsMoth <67247209+RsMoth@users.noreply.github.com>
2020-09-28 19:09:04 +0000

    Preparing release 0.2

--- include/strcasestr.h
+++ include/strcasestr.h
@@ -2,4 +2,7 @@
  * Copyright (c) 1990, 1993
  *	The Regents of the University of California.  All rights reserved.
  *
- * This code i
+ * This code is derived from software contributed to Berkeley by
+ * Chris Torek.
+ *
+ * Redistribution and use in sour
