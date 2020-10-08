d170debc9b7fbca65683747379af61b329033ed9
RsMoth <67247209+RsMoth@users.noreply.github.com>
2020-10-08 10:48:26 +0000

    css files

--- include/strcasestr.h
+++ include/strcasestr.h
@@ -12,4 +12,5 @@
  *    notice, this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
- *    documentation an
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning feat
