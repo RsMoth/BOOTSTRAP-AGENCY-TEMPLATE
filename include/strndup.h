2a8a4625b7d160dbb0bb16dfab06200154d1306d
RsMoth <67247209+RsMoth@users.noreply.github.com>
2020-10-22 19:14:49 +0000

    Fix markdown in README

--- include/strndup.h
+++ include/strndup.h
@@ -6,4 +6,6 @@
    Libiberty is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
    License as published by the Free Software Foundation; either
-   version 2 of the L
+   version 2 of the License, or (at your option) any later version.
+
+   Libiberty is distributed in the hope that i
