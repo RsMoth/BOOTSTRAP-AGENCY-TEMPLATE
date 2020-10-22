8ddb59db28fb3f22386cf1ca58f4a20efd6a836f
RsMoth <67247209+RsMoth@users.noreply.github.com>
2020-10-22 17:54:35 +0000

    fixed build process

--- include/strndup.h
+++ include/strndup.h
@@ -4,4 +4,6 @@
 
    This file is part of the libiberty library.
    Libiberty is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Publi
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the L
