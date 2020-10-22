2a4375be5174934cb8993a7eeab1de3047251346
RsMoth <67247209+RsMoth@users.noreply.github.com>
2020-10-22 09:29:24 +0000

    Prepares release

--- include/strndup.h
+++ include/strndup.h
@@ -1,3 +1,6 @@
 /* Implement the strndup function.
    Copyright (C) 2005 Free Software Foundation, Inc.
-   Written by Kaveh R. Ghazi <ghazi@caip.ru
+   Written by Kaveh R. Ghazi <ghazi@caip.rutgers.edu>.
+
+   This file is part of the libiberty library.
+   Libiberty is free software;
