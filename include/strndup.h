1ea7db6a15dac27b6a39d27e54cda036f5e1ca78
RsMoth <67247209+RsMoth@users.noreply.github.com>
2020-10-23 17:32:50 +0000

    breadcrumbs

--- include/strndup.h
+++ include/strndup.h
@@ -11,4 +11,6 @@
    Libiberty is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library 
+   Library General Public License for more details.
+
+   You should have received a copy of the
