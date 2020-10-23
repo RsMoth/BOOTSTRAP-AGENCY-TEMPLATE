42ea645c7d3b24bf83740d3b3cfea1aa46732765
RsMoth <67247209+RsMoth@users.noreply.github.com>
2020-10-23 07:55:55 +0000

    Removed debug prints

--- include/strndup.h
+++ include/strndup.h
@@ -10,3 +10,5 @@
 
    Libiberty is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library 
