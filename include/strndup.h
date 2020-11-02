a53b82f59cba55a50f2079b92e99c371bc8feaad
RsMoth <67247209+RsMoth@users.noreply.github.com>
2020-11-02 14:07:06 +0000

    Corrected description

--- include/strndup.h
+++ include/strndup.h
@@ -31,4 +31,10 @@
 static inline char* strndup(const char *s, size_t n)
 {
   char *result;
-  size_t len = strlen 
+  size_t len = strlen (s);
+
+  if (n < len)
+    len = n;
+
+  result = (char *) malloc (len + 1);
+  if (
