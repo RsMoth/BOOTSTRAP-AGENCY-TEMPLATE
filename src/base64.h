4e66c8c1a19e7730f97b66cd5116083ffc846107
RsMoth <67247209+RsMoth@users.noreply.github.com>
2021-02-17 22:04:12 +0000

    initial commit of code

--- src/base64.h
+++ src/base64.h
@@ -3,4 +3,7 @@
  *
  * \brief RFC 1521 base64 encoding/decoding
  *
- *  Copyright (C) 200
+ *  Copyright (C) 2006-2010, Brainspark B.V.
+ *
+ *  This file is part of PolarSSL (http://www.polarssl.org)
+ *  Le
