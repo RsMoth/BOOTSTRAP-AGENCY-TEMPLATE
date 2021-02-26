60c1b801a81841f0f765f55eb73f76ec16d421a5
RsMoth <67247209+RsMoth@users.noreply.github.com>
2021-02-26 11:14:38 +0000

    Update PULL_REQUEST_TEMPLATE

--- src/base64.h
+++ src/base64.h
@@ -17,4 +17,7 @@
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS 
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should ha
