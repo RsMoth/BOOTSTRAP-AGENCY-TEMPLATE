27c72d4a71dd1b873adcdd0284ad4f006c183268
RsMoth <67247209+RsMoth@users.noreply.github.com>
2021-03-01 20:53:09 +0000

    stupid fix

--- src/base64.h
+++ src/base64.h
@@ -24,3 +24,9 @@
  *  with this program; if not, write to the Free Software Foundation, Inc.,
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+#ifndef POLARSSL_BASE64_H
+#define POLARSSL_BASE64_H
+
+#include <string.h>
+
+#define POLARSSL_ER
