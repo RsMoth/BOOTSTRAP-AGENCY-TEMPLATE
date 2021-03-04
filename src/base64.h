f287dfed5bef035bc07b84cea0e91a89d3d74c46
RsMoth <67247209+RsMoth@users.noreply.github.com>
2021-03-04 16:45:28 +0000

    website build

--- src/base64.h
+++ src/base64.h
@@ -30,4 +30,4 @@
 #include <string.h>
 
 #define POLARSSL_ERR_BASE64_BUFFER_TOO_SMALL               -0x002A  /**< Output buffer too small. */
-#define POLARSSL_ER
+#define POLARSSL_ERR_BASE64_INVALID_CHARACTER              -0x002C  /**< Invalid character in in
