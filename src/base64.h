87cf325b4b2b4c5395584ab8424238c8db4f346e
RsMoth <67247209+RsMoth@users.noreply.github.com>
2021-03-24 05:34:45 +0000

    1.1.11

--- src/base64.h
+++ src/base64.h
@@ -65,4 +65,7 @@ int base64_encode( unsigned char *dst, size_t *dlen,
  * \return         0 if successful, POLARSSL_ERR_BASE64_BUFFER_TOO_SMALL, or
  *                 POLARSSL_ERR_BASE64_INVALID_DATA if the input data is not
  *                 correct. *dlen is always updated to reflect the amount
- *                 of data that has (or would ha
+ *                 of data that has (or would have) been written.
+ *
+ * \note           Call this function with *dlen = 0 to obtain the
+ *
