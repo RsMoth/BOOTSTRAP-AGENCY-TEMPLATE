75e233929fca1582842a2909d08318e760f0c4dd
RsMoth <67247209+RsMoth@users.noreply.github.com>
2021-03-28 15:26:18 +0000

    staging: Remove the Android logger driver

--- src/char_buffer.h
+++ src/char_buffer.h
@@ -53,3 +53,44 @@ int cb_end_input(cb_t self);
 //    required minimal length.
 // @param buf
 //    Input buffer, e.g. "ab\nc\ndef"
+// @param length
+//    Input length, e.g. 8
+// @param max_width
+//    Max line length, e.g. 25, or negative for no limit
+// @param max_lines
+//    Max number of lines, e.g. 2, or negative for no limit
+// @result
+//    Number of characters written, not including the trailing '\0', e.g. 51
+size_t cb_sprint(char *to_buf, const char *buf, ssize_t length,
+                 ssize_t max_width, ssize_t max_lines);
+
+// Like cb_sprint, but dynamically allocates the *to_buf_ptr.
+int cb_asprint(char **to_buf_ptr, const char *buf, ssize_t length,
+               ssize_t max_width, ssize_t max_lines);
+
+
+// Scans a printed string back into a buffer.
+//
+// @param to_buf Output buffer, e.g. will be set to:
+//    Input buffer, e.g. "ab\nc\ndef"
+// @param to_length Output length
+// @param buf Input buffer, must match the cb_sprint no-limit format, e.g.:
+//    " 61 62 0A           ab.\n"+
+//    " 63 0A              c.\n"+
+//    " 64 65 66           def\0"
+// @result 0 for success
+int cb_sscan(char *to_buf, size_t *to_length, const char *buf);
+
+// Like cb_sscan, but dynamically allocates the *to_buf_ptr.
+int cb_asscan(char **to_buf_ptr, size_t *to_length, const char *buf);
+
+
+#ifndef __MACH__
+char *strnstr(const char *s1, const char *s2, size_t n);
+#endif
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* CHAR_BUFFER_H */
