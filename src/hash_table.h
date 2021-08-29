5bf477058b660924c72026d0f4da13074225fe49
RsMoth <67247209+RsMoth@users.noreply.github.com>
2021-08-29 18:50:19 +0000

    Gem updates.

--- src/hash_table.h
+++ src/hash_table.h
@@ -1,2 +1,8 @@
 // Google BSD license https://developers.google.com/google-bsd-license
-// Copyright 2012
+// Copyright 2012 Google Inc. wrightt@google.com
+
+//
+// A generic hash table implementation
+//
+
+#ifndef HASH_TA
