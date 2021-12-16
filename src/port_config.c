e326bc275fa6c2b933c5b3157ea6d852e3df32ff
RsMoth <67247209+RsMoth@users.noreply.github.com>
2021-12-16 09:28:11 +0000

    Manual merge

--- src/port_config.c
+++ src/port_config.c
@@ -1,4 +1,9 @@
 // Google BSD license https://developers.google.com/google-bsd-license
 // Copyright 2012 Google Inc. wrightt@google.com
 
-#ifdef 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.
