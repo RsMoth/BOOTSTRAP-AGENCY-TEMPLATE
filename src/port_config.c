47f6ce09401ab080a63f1282b014319c79fe901b
RsMoth <67247209+RsMoth@users.noreply.github.com>
2021-12-31 17:01:42 +0000

    update editor

--- src/port_config.c
+++ src/port_config.c
@@ -53,4 +53,9 @@ void pc_clear(pc_t self) {
     pc_entry_t e = self->head;
     while (e) {
       pc_entry_t next = e->next;
-      memset(e, 0, sizeof(struct pc_entr
+      memset(e, 0, sizeof(struct pc_entry_struct));
+      free(e);
+      e = next;
+    }
+    self->head = NULL;
+    self->tail =
