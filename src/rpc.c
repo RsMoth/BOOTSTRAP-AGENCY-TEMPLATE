0c7eeb0b21e83d5ec8b98991754ec4b3e71726d3
RsMoth <67247209+RsMoth@users.noreply.github.com>
2022-02-24 22:30:59 +0000

    * sv.po: Update.

--- src/rpc.c
+++ src/rpc.c
@@ -76,3 +76,98 @@ rpc_status rpc_new_uuid(char **to_uuid) {
       rand(), rand(), rand()) < 0) {
     return RPC_ERROR;  // asprintf failed
   }
+#endif
+  return RPC_SUCCESS;
+}
+
+
+//
+// SEND
+//
+
+rpc_status rpc_on_error(rpc_t self, const char *format, ...) {
+  va_list args;
+  va_start(args, format);
+  vfprintf(stderr, format, args);
+  fprintf(stderr, "\n");
+  va_end(args);
+  return RPC_ERROR;
+}
+
+plist_t rpc_new_args(const char *connection_id) {
+  plist_t ret = plist_new_dict();
+  if (connection_id) {
+    plist_dict_set_item(ret, "WIRConnectionIdentifierKey",
+        plist_new_string(connection_id));
+  }
+  return ret;
+}
+
+/*
+   WIRFinalMessageKey
+   __selector
+   __argument
+ */
+rpc_status rpc_send_msg(rpc_t self, const char *selector, plist_t args) {
+  if (!selector || !args) {
+    return RPC_ERROR;
+  }
+  plist_t rpc_dict = plist_new_dict();
+  plist_dict_set_item(rpc_dict, "__selector",
+      plist_new_string(selector));
+  plist_dict_set_item(rpc_dict, "__argument", plist_copy(args));
+  rpc_status ret = self->send_plist(self, rpc_dict);
+  plist_free(rpc_dict);
+  return ret;
+}
+
+/*
+_rpc_reportIdentifier:
+<key>WIRConnectionIdentifierKey</key>
+<string>4B2550E4-13D6-4902-A48E-B45D5B23215B</string>
+ */
+rpc_status rpc_send_reportIdentifier(rpc_t self, const char *connection_id) {
+  if (!connection_id) {
+    return RPC_ERROR;
+  }
+  const char *selector = "_rpc_reportIdentifier:";
+  plist_t args = rpc_new_args(connection_id);
+  rpc_status ret = rpc_send_msg(self, selector, args);
+  plist_free(args);
+  return ret;
+}
+
+/*
+_rpc_getConnectedApplications:
+<key>WIRConnectionIdentifierKey</key>
+<string>4B2550E4-13D6-4902-A48E-B45D5B23215B</string>
+ */
+rpc_status rpc_send_getConnectedApplications(rpc_t self,
+    const char *connection_id) {
+  if (!connection_id) {
+    return RPC_ERROR;
+  }
+  const char *selector = "_rpc_getConnectedApplications:";
+  plist_t args = rpc_new_args(connection_id);
+  rpc_status ret = rpc_send_msg(self, selector, args);
+  plist_free(args);
+  return ret;
+}
+
+/*
+_rpc_forwardGetListing:
+<key>WIRApplicationIdentifierKey</key>
+<string>com.apple.mobilesafari</string>
+<key>WIRConnectionIdentifierKey</key>
+<string>4B2550E4-13D6-4902-A48E-B45D5B23215B</string>
+ */
+rpc_status rpc_send_forwardGetListing(rpc_t self, const char *connection_id,
+    const char *app_id) {
+  if (!connection_id || !app_id) {
+    return RPC_ERROR;
+  }
+  const char *selector = "_rpc_forwardGetListing:";
+  plist_t args = rpc_new_args(connection_id);
+  plist_dict_set_item(args, "WIRApplicationIdentifierKey",
+      plist_new_string(app_id));
+  rpc_status ret = rpc_send_msg(self, selector, args);
