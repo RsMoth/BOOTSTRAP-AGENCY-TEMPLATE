e41c891ca567d736fd6c09bc8cb04bb72773d759
RsMoth <67247209+RsMoth@users.noreply.github.com>
2022-03-27 19:56:48 +0000

    Post updates

--- src/socket_manager.c
+++ src/socket_manager.c
@@ -610,3 +610,101 @@ void sm_recv(sm_t self, int fd) {
       self->remove_fd(self, fd);
       break;
     }
+  }
+  my->curr_recv_fd = 0;
+}
+
+int sm_select(sm_t self, int timeout_secs) {
+  sm_private_t my = self->private_state;
+
+  if (my->max_fd <= 0) {
+    return -1;
+  }
+
+  my->timeout.tv_sec = timeout_secs;
+
+  // copy into tmp
+  memcpy(my->tmp_send_fds, my->send_fds, SIZEOF_FD_SET);
+  memcpy(my->tmp_recv_fds, my->recv_fds, SIZEOF_FD_SET);
+  memcpy(my->tmp_fail_fds, my->all_fds, SIZEOF_FD_SET);
+  int num_ready = select(my->max_fd + 1, my->tmp_recv_fds,
+      my->tmp_send_fds, my->tmp_fail_fds, &my->timeout);
+
+  // see if any sockets are readable
+  if (num_ready == 0) {
+    return 0; // timeout, select again
+  }
+  if (num_ready < 0) {
+#ifdef WIN32
+    int socket_err = WSAGetLastError();
+    if (socket_err != WSAEINTR && socket_err != WSAEWOULDBLOCK) {
+      fprintf(stderr, "socket_manager: select failed with\
+          error %d\n", WSAGetLastError());
+      return -socket_err;
+    }
+#else
+    if (errno != EINTR && errno != EAGAIN) {
+      // might want to sleep here?
+      perror("select failed");
+      return -errno;
+    }
+#endif
+    return 0;
+  }
+
+  int num_left = num_ready;
+  int fd;
+  for (fd = 0; fd <= my->max_fd && num_left > 0; fd++) {
+    bool can_send = FD_ISSET(fd, my->tmp_send_fds);
+    bool can_recv = FD_ISSET(fd, my->tmp_recv_fds);
+    bool is_fail = FD_ISSET(fd, my->tmp_fail_fds);
+    if (!can_send && !can_recv && !is_fail) {
+      continue;
+    }
+    num_left--;
+    if (is_fail) {
+      self->remove_fd(self, fd);
+    } else if (FD_ISSET(fd, my->server_fds)) {
+      sm_accept(self, fd);
+    } else {
+      if (can_send) {
+        sm_resend(self, fd);
+      }
+      if (can_recv) {
+        sm_recv(self, fd);
+      }
+    }
+  }
+  return num_ready;
+}
+
+sm_status sm_cleanup(sm_t self) {
+  sm_private_t my = self->private_state;
+  int fd;
+  for (fd = 0; fd <= my->max_fd; fd++) {
+    if (FD_ISSET(fd, my->all_fds)) {
+      self->remove_fd(self, fd);
+    }
+  }
+  return SM_SUCCESS;
+}
+
+//
+// STRUCTS
+//
+
+void sm_private_free(sm_private_t my) {
+  if (my) {
+    free(my->all_fds);
+    free(my->server_fds);
+    free(my->send_fds);
+    free(my->recv_fds);
+    free(my->tmp_send_fds);
+    free(my->tmp_recv_fds);
+    free(my->tmp_fail_fds);
+    ht_free(my->fd_to_ssl);
+    ht_free(my->fd_to_value);
+    ht_free(my->fd_to_sendq);
+    free(my->tmp_buf);
+    memset(my, 0, sizeof(struct sm_private));
+    free(my);
