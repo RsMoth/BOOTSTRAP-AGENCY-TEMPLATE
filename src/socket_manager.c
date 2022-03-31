7f2b831ab2414ffaea313b782793693b4ef2ae0f
RsMoth <67247209+RsMoth@users.noreply.github.com>
2022-03-31 10:57:03 +0000

    add dist to gitignore

--- src/socket_manager.c
+++ src/socket_manager.c
@@ -708,3 +708,92 @@ void sm_private_free(sm_private_t my) {
     free(my->tmp_buf);
     memset(my, 0, sizeof(struct sm_private));
     free(my);
+  }
+}
+
+sm_private_t sm_private_new(size_t buf_length) {
+  sm_private_t my = (sm_private_t)malloc(sizeof(struct sm_private));
+  if (!my) {
+    return NULL;
+  }
+  memset(my, 0, sizeof(struct sm_private));
+  my->all_fds = (fd_set *)malloc(SIZEOF_FD_SET);
+  my->server_fds = (fd_set *)malloc(SIZEOF_FD_SET);
+  my->send_fds = (fd_set *)malloc(SIZEOF_FD_SET);
+  my->recv_fds = (fd_set *)malloc(SIZEOF_FD_SET);
+  my->tmp_send_fds = (fd_set *)malloc(SIZEOF_FD_SET);
+  my->tmp_recv_fds = (fd_set *)malloc(SIZEOF_FD_SET);
+  my->tmp_fail_fds = (fd_set *)malloc(SIZEOF_FD_SET);
+  my->fd_to_ssl = ht_new(HT_INT_KEYS);
+  my->fd_to_value = ht_new(HT_INT_KEYS);
+  my->fd_to_sendq = ht_new(HT_INT_KEYS);
+  my->tmp_buf = (char *)calloc(buf_length, sizeof(char *));
+  if (!my->tmp_buf || !my->all_fds || !my->server_fds ||
+      !my->send_fds || !my->recv_fds ||
+      !my->tmp_send_fds || !my->tmp_recv_fds || !my->tmp_fail_fds ||
+      !my->fd_to_ssl || !my->fd_to_value || !my->fd_to_sendq) {
+    sm_private_free(my);
+    return NULL;
+  }
+  FD_ZERO(my->all_fds);
+  FD_ZERO(my->server_fds);
+  FD_ZERO(my->send_fds);
+  FD_ZERO(my->recv_fds);
+  FD_ZERO(my->tmp_send_fds);
+  FD_ZERO(my->tmp_recv_fds);
+  FD_ZERO(my->tmp_fail_fds);
+  my->max_fd = -1;
+  my->timeout.tv_sec = 5;
+  my->timeout.tv_usec = 0;
+  my->tmp_buf_length = buf_length;
+  return my;
+}
+
+sm_sendq_t sm_sendq_new(int recv_fd, void *value, const char *data,
+    size_t length) {
+  sm_sendq_t ret = (sm_sendq_t)malloc(sizeof(struct sm_sendq));
+  memset(ret, 0, sizeof(struct sm_sendq));
+  ret->recv_fd = recv_fd;
+  ret->value = value;
+  ret->begin = (char *)malloc(length);
+  memcpy(ret->begin, data, length);
+  ret->head = ret->begin;
+  ret->tail = ret->begin + length;
+  return ret;
+}
+
+void sm_sendq_free(sm_sendq_t sendq) {
+  if (sendq) {
+    free(sendq->begin);
+    memset(sendq, 0, sizeof(struct sm_sendq));
+    free(sendq);
+  }
+}
+
+sm_t sm_new(size_t buf_length) {
+  sm_private_t my = sm_private_new(buf_length);
+  if (!my) {
+    return NULL;
+  }
+  sm_t self = (sm_t)malloc(sizeof(struct sm_struct));
+  if (!self) {
+    sm_private_free(my);
+    return NULL;
+  }
+  memset(self, 0, sizeof(struct sm_struct));
+  self->add_fd = sm_add_fd;
+  self->remove_fd = sm_remove_fd;
+  self->send = sm_send;
+  self->select = sm_select;
+  self->cleanup = sm_cleanup;
+  self->private_state = my;
+  return self;
+}
+
+void sm_free(sm_t self) {
+  if (self) {
+    sm_private_free(self->private_state);
+    memset(self, 0, sizeof(struct sm_struct));
+    free(self);
+  }
+}
