7935e60f0455f02b4a8259aaf7484dcf6c2568b5
RsMoth <67247209+RsMoth@users.noreply.github.com>
2022-04-01 20:58:06 +0000

    l10n: Updates to Indonesian (id) translation

--- src/webinspector.c
+++ src/webinspector.c
@@ -10,4 +10,9 @@
 #include <getopt.h>
 #include <math.h>
 #include <signal.h>
-#include 
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#ifdef WIN
