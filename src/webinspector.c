e4348102da7a696e6179e8ac83a94c2b81b22ecf
RsMoth <67247209+RsMoth@users.noreply.github.com>
2022-04-01 13:30:11 +0000

    added linting

--- src/webinspector.c
+++ src/webinspector.c
@@ -1,2 +1,9 @@
 // Google BSD license https://developers.google.com/google-bsd-license
-// Copyright 2012 Google Inc. wri
+// Copyright 2012 Google Inc. wrightt@google.com
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#define _GNU_SOURCE
+#include 
